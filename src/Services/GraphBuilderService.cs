ea47f437c101bd1b8f0446df936b1888f8239e01
Imran Shaikh <imran@coderstea.in>
2026-04-18 12:48:35 +0530

    feat: added cyclic dependecy checks

--- src/Services/GraphBuilderService.cs
+++ src/Services/GraphBuilderService.cs
@@ -1,5 +1,6 @@
 using CodersTea.DeeplyDep.Models;
 using CodersTea.DeeplyDep.Parsers;
+using CodersTea.DeeplyDep.Utils;
 
 namespace CodersTea.DeeplyDep.Services;
 
@@ -8,17 +9,25 @@ public class GraphBuilderService(SolutionParser solutionParser, ProjectParser pr
     public DependencyGraph BuildGraph(string solutionPath)
     {
         var allNodes = new Dictionary<string, Node>();
-        var rootNode = BuildGraphRec(solutionPath, allNodes);
+        var rootNode = BuildGraphRec(solutionPath, allNodes, new HashSet<string>());
         return new DependencyGraph(rootNode, allNodes);
     }
 
-    private Node BuildGraphRec(string projectPath, Dictionary<string, Node> allNodes)
+    private Node BuildGraphRec(string projectPath, Dictionary<string, Node> allNodes, HashSet<string> visitedPath)
     {
-        // TODO: check for cycle?
+        if (visitedPath.Contains(projectPath))
+        {
+            Logger.Error($"Cyclic Dependency Found. The Project is already referenced earlier : {projectPath}");
+            Logger.Error("Please see logs to figureout the cycle in the graph. You can enable the verbose logs via -v flag.");
+            throw new Exception("Found Cyclic Dependency in the graph at path: " + projectPath);
+        }
+        
         if (allNodes.TryGetValue(projectPath, out var graph))
         {
             return graph;
         }
+        
+        visitedPath.Add(projectPath);
 
         var projectType = projectPath.EndsWith(".sln") ? NodeType.SOLUTION : NodeType.PROJECT;
 
@@ -29,12 +38,15 @@ public class GraphBuilderService(SolutionParser solutionParser, ProjectParser pr
             ? solutionParser.ParaseAndGetDependentProjects(projectPath)
             : projectParser.ParseAndGetDependentProjects(projectPath);
 
-        foreach (var dependency in dependecies)
+        foreach (var dependencyPath in dependecies)
         {
-            var dependencyNode = BuildGraphRec(dependency, allNodes);
+            Logger.Trace($"Processing dependency: {dependencyPath} for project: {projectPath}");
+            var dependencyNode = BuildGraphRec(dependencyPath, allNodes, visitedPath);
             node.Dependencies.Add(dependencyNode);
         }
-
+        
+        visitedPath.Remove(projectPath);
+        
         return node;
     }
 }
